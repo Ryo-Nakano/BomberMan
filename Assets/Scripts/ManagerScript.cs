23c2c5b688006d564864080b2f2d828f0ca9553f
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-07-14 16:04:46 +0900

    ▶︎ゲーム開始と同時にStageが作られる実装

--- Assets/Scripts/ManagerScript.cs
+++ Assets/Scripts/ManagerScript.cs
@@ -8,14 +8,30 @@ using UniRx.Triggers;
 public class ManagerScript : MonoBehaviour {
 
 	[SerializeField] Text scoreText;
+	[SerializeField] GameObject block;
 
 	public int score;//スコア計測用の変数
 
 	// Use this for initialization
 	void Start () {
+		BlockGenerator();
+
 		this.ObserveEveryValueChanged(_ => score)//変数scoreの値が変化する度にプッシュ
 		    .Subscribe(s => {//sに受け取った値が入ってる
 			Debug.Log("Score : " + s);
 		});//
+	}
+
+    //Stage上のBlockを生成する関数
+	void BlockGenerator()
+	{
+		for (float z = 7.5f; z > -10; z -= 2.5f)
+		{
+			for (float x = 7.5f; x > -10; x -= 2.5f)
+			{
+				var instantiatePos = new Vector3(x, 0.5f, z);
+				Instantiate(block, instantiatePos, Quaternion.identity);
+			}
+		}
 	}
 }
