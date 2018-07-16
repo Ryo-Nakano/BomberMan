393bab2b8cb10feff7c19ba8230ee8d91a940425
Ryohei Nanano <matsuhara.9999ryo@gmail.com>
2018-07-16 15:25:17 +0900

    ▶︎爆風が壁とかに当たると消えるようには実装 ・今の実装のままだとbombPowerが上がった時に爆風が壁を貫通してしまう →これスクリプトで爆風生成前に壁の有無判定するような工夫要るかもな...

--- Assets/Scripts/BlastElementScript.cs
+++ Assets/Scripts/BlastElementScript.cs
@@ -10,6 +10,7 @@ public class BlastElementScript : MonoBehaviour {
 
 	void Start () {
 		ColPlayer();//Playerに当たった時呼ばれる関数
+		ColBlock();//Blockに当たった時に呼ばれる関数
 	}
 
 	//Playerに当たった時呼ばれる関数
@@ -18,10 +19,22 @@ public class BlastElementScript : MonoBehaviour {
         this.OnTriggerEnterAsObservable()
             .TakeUntilDestroy(this)
             .Where(col => col.gameObject.tag == "Player")//当たった相手のtagが"Player"だった時だけプッシュ
-		    .Subscribe(col => 
-		    { //購読(colに当たった相手の情報入ってる)
+		    .Subscribe(col => //購読(colに当たった相手の情報入ってる)
+		    { 
 			    Destroy(col.gameObject);//当たったGemをDestroy
             });
     }
+
+    //Blockに当たった時に呼ばれる関数
+	void ColBlock()
+	{
+		this.OnTriggerEnterAsObservable()
+            .TakeUntilDestroy(this)
+            .Where(col => col.gameObject.tag == "Block")//当たった相手のtagが"Player"だった時だけプッシュ
+		    .Subscribe(_ =>//購読
+            { 
+			    Destroy(this.gameObject);//当たったGemをDestroy
+            });
+	}
 
 }
